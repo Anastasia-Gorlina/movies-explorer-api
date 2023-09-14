704a8b5917af4be0a3ac2d5500da94d7717232fc
Anastasia Gorlina <gobi199@yandex.ru>
2023-09-14 15:02:36 +0300

    fix

--- app.js
+++ app.js
@@ -41,23 +41,17 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
+
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-  }
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  // завершаем обработку запроса и возвращаем результат клиенту
+  res.header('Access-Control-Allow-Headers', requestHeaders);
+  // завершаем обработку запроса и возвращаем результат клиенту
+  
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
     res.end();
     return;
   }
