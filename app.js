8d4955349db884ea94895da40369b769fadeb705
Anastasia Gorlina <gobi199@yandex.ru>
2023-09-14 14:11:55 +0300

    fix

--- app.js
+++ app.js
@@ -45,15 +45,14 @@ app.use((req, res, next) => {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
   }
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    res.header('Access-Control-Allow-Headers', requestHeaders);
     // завершаем обработку запроса и возвращаем результат клиенту
     res.end();
     return;
